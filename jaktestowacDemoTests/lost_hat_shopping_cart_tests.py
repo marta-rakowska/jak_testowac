489091b5f51e58781695f9dfe1d26df042cf2adb
marta-rakowska <marta.rakowska91@gmail.com>
2024-06-24 19:32:54 +0200

    add files

---
+++ jaktestowacDemoTests/lost_hat_shopping_cart_tests.py
@@ -0,0 +1,42 @@
+import time
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+class LostHatShoppingCartTests(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        self.base_url = 'https://autodemo.testoneo.com/en/'
+        self.subpage_art_url = 'https://autodemo.testoneo.com/en/9-art'
+        self.service = Service(
+            '/Users/martarakowska/Desktop/podstawy_testow_automatycznych_w_selenium_i_python/chromedriver')
+        self.service.start()
+        self.driver = webdriver.Remote(self.service.service_url)
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_adding_item_to_shopping_cart(self):
+        expected_confirmation_modal_text = '\ue876Product successfully added to your shopping cart'
+        item_xpath = '//*[@alt="Mountain fox - Vector graphics"]'
+        shopping_cart_button_xpath = '//*[@class="btn btn-primary add-to-cart"]'
+        confirmation_modal_title_xpath = '//*[@id="myModalLabel"]'
+
+        driver = self.driver
+        driver.get(self.subpage_art_url)
+
+        item_element = driver.find_element(By.XPATH, item_xpath)
+        item_element.click()
+        shopping_cart_button_element = driver.find_element(By.XPATH, shopping_cart_button_xpath)
+        shopping_cart_button_element.click()
+
+        time.sleep(3)
+
+        confirmation_modal_element = driver.find_element(By.XPATH, confirmation_modal_title_xpath)
+
+        self.assertEqual(expected_confirmation_modal_text, confirmation_modal_element.text)
+
+
+
---
+++ jaktestowacDemoTests/testsuite_sanity_tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+from lost_hat_login_page_tests import LostHatLoginPageTests
+
+def sanity_suite():
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(LostHatLoginPageTests('test_correct_login'))
+    return test_suite
+
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(sanity_suite())
