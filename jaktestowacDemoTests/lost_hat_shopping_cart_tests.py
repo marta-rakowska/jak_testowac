3361e9eac39a44b3a569f02a3da232df360e092d
marta-rakowska <marta.rakowska91@gmail.com>
2024-06-26 21:19:24 +0200

    wait update

--- jaktestowacDemoTests/lost_hat_shopping_cart_tests.py
+++ jaktestowacDemoTests/lost_hat_shopping_cart_tests.py
@@ -2,13 +2,14 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from jaktestowacDemoTests.helpers import operational_helpers as oh
 
 class LostHatShoppingCartTests(unittest.TestCase):
     @classmethod
     def setUp(self):
-        self.base_url = 'https://autodemo.testoneo.com/en/'
         self.subpage_art_url = 'https://autodemo.testoneo.com/en/9-art'
         self.service = Service(
             '/Users/martarakowska/Desktop/podstawy_testow_automatycznych_w_selenium_i_python/chromedriver')
@@ -33,8 +34,11 @@ class LostHatShoppingCartTests(unittest.TestCase):
         shopping_cart_button_element = driver.find_element(By.XPATH, shopping_cart_button_xpath)
         shopping_cart_button_element.click()
 
-        confirmation_modal_elements = oh.wait_for_elements(driver, confirmation_modal_title_xpath)
-        confirmation_modal_element = confirmation_modal_elements[0]
+        # confirmation_modal_elements = oh.wait_for_elements(driver, confirmation_modal_title_xpath)
+        # confirmation_modal_element = confirmation_modal_elements[0]
+        confirmation_modal_element = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, confirmation_modal_title_xpath)),
+            f"Element for xpath: '{confirmation_modal_title_xpath}' and url: {driver.current_url} not found")
         self.assertEqual(expected_confirmation_modal_text, confirmation_modal_element.text)
 
 
