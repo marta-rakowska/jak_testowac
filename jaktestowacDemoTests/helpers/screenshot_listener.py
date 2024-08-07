daf940ab82a6e4b348d56f188faccbfd8f42b52f
marta-rakowska <marta.rakowska91@gmail.com>
2024-08-07 18:27:19 +0200

    make_screenshot refactor

--- jaktestowacDemoTests/helpers/screenshot_listener.py
+++ jaktestowacDemoTests/helpers/screenshot_listener.py
@@ -1,5 +1,6 @@
 import time
 import allure
+import os
 from allure_commons.types import AttachmentType
 from selenium.webdriver.support.events import AbstractEventListener
 
@@ -14,8 +15,10 @@ class ScreenshotListener(AbstractEventListener):
         if str(exception) not in pycharm_debugger_exceptions:
             make_screenshot(driver, 'driver')
 
+
 def make_screenshot(driver, producer):
-    screenshot_path = rf"testResults/{producer}_exception_{time.time()}.png"
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    screenshot_path = rf"{dir_path}/../testResults/{producer}_exception_{time.time()}.png"
     driver.get_screenshot_as_file(screenshot_path)
     allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
     print(f"Screenshot saved as {screenshot_path}")
